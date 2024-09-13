36096f76e55b0d0a1fcc844edabcddd862923590
Micael Oliveira <micaeloliveira.tab@gmail.com>
2024-09-13 04:36:19 -0300

    crash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+ def index
+   @questionarios = questionario.order(created_at: :desc)
+   render json: @questionarios
+ end
 end
---
+++ app/controllers/questionarios_controller.rb
@@ -0,0 +1,2 @@
+class QuestionariosController < ApplicationController
+end
